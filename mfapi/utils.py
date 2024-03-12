ba1734986abbfe1715b6b453de3d6f389a45dae4
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2024-03-12 11:25:24 +0100

    can create and destroy different temp folders

--- mfapi/utils.py
+++ mfapi/utils.py
@@ -7,17 +7,17 @@ from pydub import AudioSegment
 import uuid
 import shutil
 
-def get_temp_file(file_ext, file_name = None):
+def get_temp_file(file_ext, file_name = None, folder_name = "temp"):
     if file_name == None:
         file_name = str(uuid.uuid4())
-    full_path = os.path.join(os.getcwd(), "temp", f"{file_name}.{file_ext}")
+    full_path = os.path.join(os.getcwd(), folder_name, f"{file_name}.{file_ext}")
     if os.path.isdir(os.path.dirname(full_path)) == False:
         os.makedirs(os.path.dirname(full_path))
     return full_path
 
-def clean_up():
-    if os.path.isdir(os.path.join(os.getcwd(), "temp")):
-        shutil.rmtree(os.path.join(os.getcwd(), "temp"))
+def clean_up(folder_name = "temp"):
+    if os.path.isdir(os.path.join(os.getcwd(), folder_name)):
+        shutil.rmtree(os.path.join(os.getcwd(), folder_name))
 
 def get_tetras_url(url):
     prefix = "https://files.tetras-libre.fr/"
@@ -40,16 +40,16 @@ def dl_range(url, range, path):
                 file.write(chunk)
 
 def get_online_video_dims(url):
-    dl_path = get_temp_file("mp4")
+    dl_path = get_temp_file("mp4", None, "temp_media")
     dl_range(url, "0-5242880", dl_path)
     vid = cv2.VideoCapture(dl_path)
     width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    clean_up()
+    clean_up("temp_media")
     return width, height
 
 def get_online_video_duration(url):
-    dl_path = get_temp_file("mp4")
+    dl_path = get_temp_file("mp4", None, "temp_media")
     dl_range(url, "0-5242880", dl_path)
     vid = cv2.VideoCapture(dl_path)
     
@@ -57,11 +57,11 @@ def get_online_video_duration(url):
     fps = vid.get(cv2.CAP_PROP_FPS)
     duration = num_frames / fps
 
-    clean_up()
+    clean_up("temp_media")
     return duration
 
 def get_online_audio_duration(url):
-    dl_path = get_temp_file("mp3")
+    dl_path = get_temp_file("mp3", None, "temp_media")
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open(dl_path, 'wb') as file:
@@ -70,11 +70,11 @@ def get_online_audio_duration(url):
         
         audio = AudioSegment.from_file(dl_path)
         duration = len(audio) / 1000.0 
-        clean_up()
+        clean_up("temp_media")
         return duration
     
 def online_img_to_np(url):
-    dl_path = get_temp_file("jpg")
+    dl_path = get_temp_file("jpg", None, "temp_media")
     response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open(dl_path, 'wb') as file:
@@ -85,7 +85,7 @@ def online_img_to_np(url):
         img = cv2.imread(_expand_path(dl_path))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        clean_up()
+        clean_up("temp_media")
         return img
 
 def _expand_path(path: str) -> str:
